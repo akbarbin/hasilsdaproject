347b8c54d917a7ceed1c8a568f78df8ee19af14d
Muhamad Akbar Bin Widayat <muhamadakbarbw@gmail.com>
2014-12-29 21:25:30 +0700

    setting database production

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'hasilsda';
+$db['default']['username'] = 'k5524053_rhsda';
+$db['default']['password'] = '3792yzzkgz';
+$db['default']['database'] = 'k5524053_rhsda';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
